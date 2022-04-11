134207d128c37b77be1cb68f8b14a8bc82fafa95
Tyler <king.tut@rogers.com>
2022-04-11 10:21:02 -0400

    added search feature to SRD monster tab

--- app/src/main/java/com/example/dndmonstereditor/fragments/MonsterListFragment.kt
+++ app/src/main/java/com/example/dndmonstereditor/fragments/MonsterListFragment.kt
@@ -15,6 +15,7 @@ import com.example.dndmonstereditor.adapter.MonsterListItemAdapter
 import com.example.dndmonstereditor.adapter.OnMonsterClickListener
 import com.example.dndmonstereditor.databinding.FragmentMonsterListBinding
 import com.example.dndmonstereditor.model.MonsterFromList
+import com.example.dndmonstereditor.model.MonstersList
 import com.example.dndmonstereditor.viewmodel.MonsterViewModel
 import com.example.dndmonstereditor.viewmodel.States
 import dagger.hilt.android.AndroidEntryPoint
@@ -34,6 +35,8 @@ class MonsterListFragment : Fragment(), OnMonsterClickListener {
         MonsterListItemAdapter(this)
     }
 
+    private lateinit var monsters: MonstersList
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -47,18 +50,40 @@ class MonsterListFragment : Fragment(), OnMonsterClickListener {
             adapter = monsterAdapter
         }
 
+        binding.searchPopOut.setOnClickListener {
+            if (binding.searchBar.visibility==View.GONE){
+                binding.searchBar.visibility=View.VISIBLE
+                binding.searchButton.visibility=View.VISIBLE
+            } else{
+                binding.searchBar.visibility=View.GONE
+                binding.searchButton.visibility=View.GONE
+            }
+        }
+
+        binding.searchButton.setOnClickListener {
+            val searchedMonsters= mutableListOf<MonsterFromList>()
+
+            for (monster in monsters.monsterFromLists){
+                if (monster.name.contains(binding.searchBar.text.toString(),true)){
+                    searchedMonsters.add(monster)
+                }
+            }
+            monsterAdapter.addToMonsterList(searchedMonsters)
+        }
+
         monsterViewModel.monstersLiveData.observe(viewLifecycleOwner) { state ->
             when(state) {
                 is States.LOADING -> {
-
+                    binding.searchPopOut.visibility=View.GONE
                     binding.MonsterList.visibility=View.GONE
                     binding.loadingImage.visibility=View.VISIBLE
                 }
                 is States.SUCCESS -> {
+                    binding.searchPopOut.visibility=View.VISIBLE
                     binding.MonsterList.visibility=View.VISIBLE
                     binding.loadingImage.visibility=View.GONE
-                    val monsters = state.response
-                        monsterAdapter.addToMonsterList(monsters.monsterFromLists)
+                    monsters = state.response
+                    monsterAdapter.addToMonsterList(monsters.monsterFromLists)
                 }
                 is States.ERROR -> {
                     Toast.makeText(requireContext(), state.error.localizedMessage, Toast.LENGTH_LONG).show()
